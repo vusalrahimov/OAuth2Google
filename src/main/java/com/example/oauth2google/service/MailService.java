c0948c0ae0c7e1f5791a1c12496283ab8107bff1
vusal <vusalr06@gmail.com>
2022-09-15 21:09:39 +0400

    Mail

---
+++ src/main/java/com/example/oauth2google/service/MailService.java
@@ -0,0 +1,11 @@
+package com.example.oauth2google.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+    private final JavaMailSender javaMailSender;
+}
